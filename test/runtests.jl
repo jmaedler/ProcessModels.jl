37646dd2ac484897ae018d9976fb4018502a5b43
jmaedler <j.maedler@gmx.net>
2024-09-16 17:13:52 +0200

    Test zur simpleBSTR eingeführt

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
 using ProcessModels
 using Test
+using ModelingToolkit
+using DifferentialEquations
+
+function test_simpleBSTR1(c_Ainit, c_Binit)
+    @mtkbuild bstr = SimpleBSTR(;k = 1)
+    u_0 = [bstr.c_A => c_Ainit, bstr.c_B => c_Binit] 
+    tspan = (0,1000)
+    prob = ODEProblem(bstr,u_0,tspan)
+    sol = solve(prob)
+    return sol       
+end
+
+sol1 = test_simpleBSTR1(0,0)
+sol2 = test_simpleBSTR1(1,0)
 
 @testset "ProcessModels.jl" begin
     # Write your tests here.
+    @test isapprox(sol1.u[end][1], 0; atol = 1e-06)
+    @test isapprox(sol1.u[end][2], 0; atol = 1e-06)
+    @test isapprox(sol2.u[end][1], 0; atol = 1e-06)
+    @test isapprox(sol2.u[end][2], 1; atol = 1e-06)
 end
